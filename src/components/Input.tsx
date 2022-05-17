c0fa1160ae032dccfe41bf695736a8e3efd5cca8
Lei Nelissen <lei@codified.nl>
2022-05-17 23:05:45 +0200

    Make padding-left optional on textinput

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,13 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-const Input = styled.TextInput`
+const Input = styled.TextInput<{ icon?: boolean }>`
     margin: 10px 0;
     border-radius: 8px;
     padding: 15px;
-    padding-left: 40px;
+
+    ${(props) => props.icon && css`
+        padding-left: 40px;
+    `}
 `;
 
 export default Input;
