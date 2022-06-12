a402757c96f818f0da68159fea26cf635073bed6
Lei Nelissen <lei@codified.nl>
2022-06-12 23:24:32 +0200

    Remove ClosePlayer

--- src/screens/modals/Player/index.tsx
+++ src/screens/modals/Player/index.tsx
@@ -20,7 +20,6 @@ export default function Player() {
                     <StreamStatus />
                     <ProgressBar />
                     <MediaControls />
-                    <ClosePlayer />
                 </>                    
             )} />
         </GestureHandlerRootView>
