2e816f4a71f563de25032daa78c1310bdc320c58
Lei Nelissen <lei@codified.nl>
2023-04-23 01:14:56 +0200

    fix: correctly calculate amount of minutes when an hour is present

--- src/utility/ticksToDuration.ts
+++ src/utility/ticksToDuration.ts
@@ -3,7 +3,7 @@ function ticksToDuration(ticks: number) {
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
 
-    return `${hours > 0 ? hours + ':' : ''}${minutes}:${(seconds % 60).toString().padStart(2, '0')}`;
+    return `${hours > 0 ? hours + ':' : ''}${(minutes % 60).toString().padStart(hours > 0 ? 2 : 0, '0')}:${(seconds % 60).toString().padStart(2, '0')}`;
 }
 
 export default ticksToDuration;
