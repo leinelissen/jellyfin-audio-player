968e98d8dffa79ea3165d1209542bd91dd914ef5
Lei Nelissen <lei@codified.nl>
2024-05-26 22:20:05 +0200

    fix: react-native-screens android setup

--- android/app/src/main/java/com/jellyfinaudioplayer/MainActivity.kt
+++ android/app/src/main/java/com/jellyfinaudioplayer/MainActivity.kt
@@ -5,6 +5,8 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.os.Bundle;
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -19,4 +21,8 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
